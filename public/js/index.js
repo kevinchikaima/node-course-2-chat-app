0fd4c07b622b09071015e69286de4527af25b527
Kevin C Ma <39309027+kevinchikaima@users.noreply.github.com>
2018-06-13 15:07:51 -0700

    emit new message on createmessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  // socket.emit('createEmail', {
-  //   to: 'jen@email.com',
-  //   text: 'Sounds good!'
-  // });
-
-  socket.emit('createMessage', {
-    from: 'Kevin',
-    text: 'Hello'
-  });
 });
 
 socket.on('disconnect', function () {
